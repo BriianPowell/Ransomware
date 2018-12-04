2adc7a9a6c5c8b6b25847d7575a3a84125ee6cc4
Brian <bapowell00@gmail.com>
2018-12-03 18:43:18 -0800

    Started Sanitation file, might archive, don't care to finish

--- RSAFileEncrypt/Sanitation.py
+++ RSAFileEncrypt/Sanitation.py
@@ -1,5 +1,40 @@
-import os, var, json
+import os, var, json, base64
+from RSAMain import RSAEncryption
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding, hashes, hmac, serialization
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding as rsa_padding
+
+'''
+ You got totally
+ ~ SAVED ~
+ by:
+ Brian Powell @BriianPowell
+ Mina Messiha @MinaMessiha109
+ CECS 378 - Cyber Security
+ Aliasgari
+'''
+
+class Sanitation:
+    def decryptDir(self):
+        currentDir = os.getcwd()
+
+        for root, dirs, files in os.walk(currentDir):
+            for file in files:
+                if file.endswith('.json'):
+                    with open(file,'r') as jFile:
+                        jData = jFile.read()
+
+                    jObj = json.loads(jData)
+                    
+
+                os.remove(os.path.join(root, file))                    
+
+
+
+
+def main():
+    Sanitation().decryptDir()
+
+if __name__ == '__main__':
+    main()
